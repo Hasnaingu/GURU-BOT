411408cf821352f46fd7b092dae88aac3af7ddd9
Hasnaingu <130880974+Hasnaingu@users.noreply.github.com>
2023-12-05 12:56:24 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['923425496901', 'GURU', true],
+  ['923012720223', 'GURU', true],
   ['923012720223', 'Ash', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['923425496901','923425496901'] 
-global.prems = ['923425496901','923425496901', '923012720223']
-global.allowed = ['923425496901','923425496901', '923012720223']
+global.mods = ['923012720223','923012720223'] 
+global.prems = ['923012720223','923012720223', '923012720223']
+global.allowed = ['923012720223','923012720223', '923012720223']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
